5f6156a310ac23aeaecf9a1f75e3b7a2e7227dbf
Ayanna Scoon-Jerome <ayanna.scoon@gmail.com>
2017-12-15 01:43:30 -0500

    fixed authentication, validated email and matching passwords

--- assets/javascript/fbase.js
+++ assets/javascript/fbase.js
@@ -14,9 +14,10 @@ firebase.initializeApp(config);
 
 // Add login event
 $("#btnLogin").on("click", function (event) {
-//const btnLogin = document.getElementbyId('btnLogin');
-//btnLogin.addEventListener('click', e +> {)
+    //const btnLogin = document.getElementbyId('btnLogin');
+    //btnLogin.addEventListener('click', e +> {)
     //Get email and password
+    event.preventDefault();
     console.log("user logging in");
     const userEmail = $("#emailInput").val().trim();
     const userPswd = $("#pswdInput").val().trim();
@@ -31,24 +32,46 @@ $("#btnLogin").on("click", function (event) {
 $("#btnSignup").on("click", function (event) {
     //Get email and password
     //TODO - VALIDATE EMAIL ADDRESS
+    event.preventDefault();
     console.log("user signing up");
     const userEmail = $("#emailInput").val().trim();
     const userPswd = $("#pswdInput").val().trim();
+    const confirm = $("#confirmInput").val().trim();
     const auth = firebase.auth();
 
-    const promise = auth.createUserWithEmailAndPassword(userEmail, userPswd);
-    promise.catch(event => console.log(event.message));
+    var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+
+    console.log("1st pswd: ", userPswd);
+    console.log("2nd pswd: ", confirm);
+
+
+    if (!testEmail.test(userEmail)) {
+        console.log("Invalid email address");
+        $("#errorDisplay").text("Uh oh... that's not a valid user address.");
+
+    } else if (userPswd !== confirm) {
+        console.log("Mismatched passwords");
+        $("#errorDisplay").text("Oops.... your passwords don't match.");
+
+    } else {
+        const promise = auth.createUserWithEmailAndPassword(userEmail, userPswd);
+        promise.catch(event => console.log(event.message));
+    }
 
 });
 
 $("#btnLogout").on("click", function (event) {
+    event.preventDefault();
     firebase.auth().signOut();
 });
-
+var currentUser;
 //Add a realtime listener
 firebase.auth().onAuthStateChanged(firebaseUser => {
+
     if (firebaseUser) {
+        currentUser = firebaseUser;
         console.log(firebaseUser);
+        console.log(firebaseUser.uid)
         $("#btnLogout").removeClass('hide');
     } else {
         console.log("not logged in");
